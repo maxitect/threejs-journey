c5f5b8bebf25ec2be6c92695ad1786238894ff9c
maxitect <maxime.downe@gmail.com>
2025-02-12 17:08:05 +0000

    chore: move cube in all axis using set vector method in first-threejs-project

--- 01_basics/first-threejs-project/src/main.ts
+++ 01_basics/first-threejs-project/src/main.ts
@@ -5,16 +5,10 @@ const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-});
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
 scene.add(mesh);
-
-mesh.position.normalize();
 
 const sizes = {
   width: 800,
