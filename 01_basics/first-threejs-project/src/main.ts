13b12c68369171df38e681200fcadf1369d187ca
maxitect <maxime.downe@gmail.com>
2025-02-17 11:52:46 +0000

    chore: reset box geometries for animation in first-threejs-project

--- 01_basics/first-threejs-project/src/main.ts
+++ 01_basics/first-threejs-project/src/main.ts
@@ -4,34 +4,12 @@ const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
 const scene = new THREE.Scene();
 
-const group = new THREE.Group();
-group.position.y = 1;
-group.scale.y = 2;
-group.rotation.y = Math.PI / 4;
-scene.add(group);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 0);
 
-const cube1 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-group.add(cube1);
-
-const cube2 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-);
-cube2.position.x = -2;
-group.add(cube2);
-
-const cube3 = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0x0000ff })
-);
-cube3.position.x = 2;
-group.add(cube3);
-
-const axesHelper = new THREE.AxesHelper();
-scene.add(axesHelper);
+scene.add(mesh);
 
 const sizes = {
   width: 800,
@@ -40,7 +18,6 @@ const sizes = {
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.set(0, 0, 3);
-// camera.lookAt(mesh.position);
 scene.add(camera);
 
 const renderer = new THREE.WebGLRenderer({
