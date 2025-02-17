92f6afc7507fc0b22c068d01f3fbea59497a9a67
maxitect <maxime.downe@gmail.com>
2025-02-17 11:41:37 +0000

    chore: transform geometry group with 3 box geometries in first-threejs-project

--- 01_basics/first-threejs-project/src/main.ts
+++ 01_basics/first-threejs-project/src/main.ts
@@ -5,6 +5,9 @@ const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 const scene = new THREE.Scene();
 
 const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = Math.PI / 4;
 scene.add(group);
 
 const cube1 = new THREE.Mesh(
