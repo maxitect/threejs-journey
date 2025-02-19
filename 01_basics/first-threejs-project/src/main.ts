3109c047b63d7eb90dbd2329afb090e4a8589186
maxitect <maxime.downe@gmail.com>
2025-02-19 12:31:25 +0000

    chore: add controls to rotate cube using mouse cursor with sin and cos in first-threejs-project

--- 01_basics/first-threejs-project/src/main.ts
+++ 01_basics/first-threejs-project/src/main.ts
@@ -1,9 +1,19 @@
 import * as THREE from "three";
-import gsap from "gsap";
 
 const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
 const scene = new THREE.Scene();
+
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  const { clientX, clientY } = event;
+  cursor.x = clientX / sizes.width - 0.5;
+  cursor.y = -(clientY / sizes.height - 0.5);
+});
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
@@ -43,6 +53,12 @@ const clock = new THREE.Clock();
 const tick = () => {
   // const elapsedTime = clock.getElapsedTime();
   // mesh.rotation.y = elapsedTime;
+
+  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  camera.position.y = cursor.y * 5;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
