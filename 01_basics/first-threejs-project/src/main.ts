fd77df91badd12202a1bed3de53537e2ab00acf1
maxitect <maxime.downe@gmail.com>
2025-02-26 10:32:07 +0000

    chore: programmaticaly generate 500 triangles within cube in first-threejs-project

--- 01_basics/first-threejs-project/src/main.ts
+++ 01_basics/first-threejs-project/src/main.ts
@@ -18,10 +18,16 @@ window.addEventListener("mousemove", (event) => {
   cursor.y = -(clientY / sizes.height - 0.5);
 });
 
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
-const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
 const geometry = new THREE.BufferGeometry();
+
+const count = 500;
+const positionsArray = new Float32Array(count * 3 * 3);
+
+for (let i = 0; i < count * 3 * 3; i++) {
+  positionsArray[i] = (Math.random() - 0.5) * 4;
+}
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
 geometry.setAttribute("position", positionsAttribute);
 
 const material = new THREE.MeshBasicMaterial({
