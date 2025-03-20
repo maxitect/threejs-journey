9cb8342aba3bce63c5374c20ecf8718f4d6fa4a3
maxitect <maxime.downe@gmail.com>
2025-03-20 11:47:23 +0000

    chore: use mesh lambert material in first-threejs-project

--- 01_basics/first-threejs-project/src/main.ts
+++ 01_basics/first-threejs-project/src/main.ts
@@ -72,7 +72,7 @@ const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement & {
 
 const scene = new THREE.Scene();
 
-const material = new THREE.MeshDepthMaterial();
+const material = new THREE.MeshLambertMaterial();
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 sphere.position.x = -1.5;
